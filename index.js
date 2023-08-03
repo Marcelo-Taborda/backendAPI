6538a2d4378c927f0af863052a647a7e6e33123f
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2023-08-03 15:28:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import imagesRoutes from "./routes/images.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200,
+    credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", imagesRoutes);
+app.use("/api", userRoutes);
+app.use("/api/uploads", express.static("uploads"));
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    }
+    )
+    .catch((error) => {
+        console.log(error.message);
+    }
+    );
